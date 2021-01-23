6c581d1d8fa4fd18df08a727167575cf407fe8b0
Anthon Wellsjö <anthonwellsjo@gmail.com>
2021-01-23 19:48:34 +0100

    Updates

---
+++ component---src-pages-categories-colomba-tsx-cccd2ee040a9f60a1f38.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[8],{SJOg:function(n,e,t){"use strict";t.r(e);var a=t("q1tI"),o=t.n(a),u=t("oI7b");e.default=function(){return o.a.createElement(u.a,null)}}}]);
+//# sourceMappingURL=component---src-pages-categories-colomba-tsx-cccd2ee040a9f60a1f38.js.map
