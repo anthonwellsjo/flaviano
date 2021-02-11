94f5178d1b4d12ff3087a44353f525be0865438f
Anthon Wellsjö <anthonwellsjo@gmail.com>
2021-02-11 18:59:59 +0100

    with materie prime page

---
+++ flaviano/src/components/Mobile/Components/MateriaPrima/MateriaPrima.tsx
@@ -0,0 +1,83 @@
+import React from 'react';
+import PageTitleDesktop from '../../../Desktop/PageTitleDesktop/PageTitleDesktop';
+import Centralizer from '../../../StructureComponents/Centralizer/Centralizer';
+import Img, { FluidObject } from 'gatsby-image';
+import classes from './MateriaPrima.module.scss';
+
+interface Props {
+  title: string,
+  fluidImg: FluidObject,
+  hexColor: string,
+  text: string,
+  imgLeft?: boolean,
+  imgBehindTitle?: boolean,
+  height: string,
+  whiteBehindImg?: boolean,
+  longTitle?: boolean
+}
+
+const MateriaPrima = ({ fluidImg, hexColor, text, title, height, imgLeft, imgBehindTitle, whiteBehindImg, longTitle }: Props) => {
+
+  if (imgLeft) return (
+    <div style={{ height: height }} className={classes.container}>
+      <div style={{ height: height, backgroundColor: hexColor }} className={classes.colorBox} />
+      <div style={{ width: "150px", height: "120px", overflow: "hidden", position: "absolute", left: "10px", marginTop: "-40px" }}>
+        <div style={{ width: "220px" }}>
+          <Img fluid={fluidImg} alt="Lievtio Madre" />
+        </div>
+      </div>
+      <div className={classes.titleContainer} style={{ right: "-5px", width: longTitle ? "80%" : "12em" }}>
+        <div style={{ height: "1px", width: "20px", backgroundColor: "black", position: "absolute", marginTop: "-5px", right: "25px" }}></div>
+        <PageTitleDesktop right letterSpacing={".2em"} fontSize={"1.5em"}>{title}</PageTitleDesktop>
+      </div>
+      <Centralizer>
+        <div className={classes.textContainer}>
+          <p className={classes.text}>{text}</p>
+        </div>
+      </Centralizer>
+    </div>
+  )
+  if (imgBehindTitle) return (
+    <div style={{ height: height }} className={classes.container}>
+      <div style={{ height: height, backgroundColor: hexColor }} className={classes.colorBox} />
+      {whiteBehindImg && <div style={{ width: "100%", height: "100px", overflow: "hidden", position: "absolute", marginTop: "0px", backgroundColor: "white" }}></div>}
+      <div style={{ width: "100%", height: "100px", overflow: "hidden", position: "absolute", marginTop: "0px", opacity: ".4" }}>
+        <div style={{ width: "110vw" }}>
+          <Img fluid={fluidImg} alt="Lievtio Madre" />
+        </div>
+      </div>
+      <div className={classes.titleContainer} style={{ marginTop: "5px" }}>
+        <div style={{ height: "1px", width: "20px", backgroundColor: "black", position: "absolute", marginTop: "-5px" }}></div>
+        <PageTitleDesktop letterSpacing={".2em"} fontSize={"1.5em"}>{title}</PageTitleDesktop>
+      </div>
+      <Centralizer>
+        <div className={classes.textContainer}>
+          <p className={classes.text}>{text}</p>
+        </div>
+      </Centralizer>
+    </div>
+  )
+
+  return (
+    <div style={{ height: height }} className={classes.container}>
+      <div style={{ height: height, backgroundColor: hexColor }} className={classes.colorBox} />
+      <div style={{ width: "150px", height: "120px", overflow: "hidden", position: "absolute", right: "10px", marginTop: "-50px" }}>
+        <div style={{ width: "220px" }}>
+          <Img fluid={fluidImg} alt="Lievtio Madre" />
+        </div>
+      </div>
+      <div className={classes.titleContainer}>
+        <div style={{ height: "1px", width: "20px", backgroundColor: "black", position: "absolute", marginTop: "-5px" }}></div>
+        <PageTitleDesktop letterSpacing={".2em"} fontSize={"1.5em"}>{title}</PageTitleDesktop>
+      </div>
+      <Centralizer>
+        <div className={classes.textContainer}>
+          <p className={classes.text}>{text}</p>
+        </div>
+      </Centralizer>
+
+    </div>
+  )
+}
+
+export default MateriaPrima;
--- flaviano/src/components/Mobile/Pages/MateriePrime/MateriePrimeMobileRender.tsx
+++ flaviano/src/components/Mobile/Pages/MateriePrime/MateriePrimeMobileRender.tsx
@@ -9,25 +9,26 @@ import Img from 'gatsby-image';
 import { MateriePrimeQuery, QuoteStyle } from '../../../../../types';
 import Centralizer from '../../../StructureComponents/Centralizer/Centralizer';
 import { useMateriePrimeQuery } from '../../../../hooks/queries/useMateriePrimeQuery';
+import MateriePrimi from '../../Components/MateriePrimi/MateriePrimi';
 
 const MateriePrimeMobileRender: React.FC = () => {
   const { sanityMateriePrimePage }: MateriePrimeQuery = useMateriePrimeQuery();
   let parallax = useRef();
   return (
     <LayoutMobile>
-      <Parallax pages={3} scrolling={true} config={{ mass: 1 }} ref={parallax}>
+      <Parallax pages={3.05} scrolling={true} config={{ mass: 1 }} ref={parallax}>
         <BackDropMobile />
-        <ParallaxLayer offset={0.} speed={0.4}>
+        <ParallaxLayer offset={0.} speed={0}>
           <LayoutHeaderMobile />
         </ParallaxLayer>
-        <ParallaxLayer offset={0.2} speed={0.15}>
+        <ParallaxLayer offset={0.2} speed={0}>
           <div style={{ width: "30px", marginTop: "-15px", textAlign: "center", position: "absolute", left: "20%", zIndex: 1 }}>
             <QuoteMobile leftQuoteX={"-30px"} leftQuoteY={"-25px"} rightQuoteX={"-5px"} rightQuoteY={"-35px"} fontSize={"4em"} style={QuoteStyle.header}>
               M
             </QuoteMobile>
           </div>
         </ParallaxLayer>
-        <ParallaxLayer offset={0.15} speed={0.2}>
+        <ParallaxLayer offset={0.15} speed={0}>
           <div style={{ width: "60%", position: "absolute", right: 0, top: "100px" }}>
             <div style={{ height: "1px", width: "15px", marginLeft: "1px", backgroundColor: "black", marginBottom: "4px" }}></div>
             <div style={{ width: "20px" }}>
@@ -35,12 +36,15 @@ const MateriePrimeMobileRender: React.FC = () => {
             </div>
           </div>
         </ParallaxLayer>
-        <ParallaxLayer offset={0.1} speed={0.1}>
+        <ParallaxLayer offset={0.1} speed={0}>
           <Centralizer>
             <div style={{ width: "75%", marginTop: "0" }}>
               <QuoteMobile left fontSize={"1.3em"} rightQuoteX={"-10px"} rightQuoteY={"70px"}>{sanityMateriePrimePage.quoteHeaderText}</QuoteMobile>
             </div>
           </Centralizer>
+        </ParallaxLayer>
+        <ParallaxLayer offset={0.97} speed={1}>
+          <MateriePrimi materiePrimi={sanityMateriePrimePage} />
         </ParallaxLayer>
       </Parallax>
     </LayoutMobile>
