d14ba644c6ae0b50493e4b1cc79b5649bca5c458
Anthon Wellsjö <anthonwellsjo@gmail.com>
2021-03-14 22:29:24 +0100

    fix gatsby build prob

--- flaviano/src/components/StructureComponents/Centralizer/Centralizer.js
+++ flaviano/src/components/StructureComponents/Centralizer/Centralizer.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import classes from './centralizer.module.scss';
+import classes from './Centralizer.module.scss';
 import classnames from 'classnames';
 
 const Centralizer = props => {
